bffeb34ad2d831a210bd7ab926718cd6a03c168d
Mads Marquart <madsmtm@gmail.com>
2018-11-18 16:33:02 +0100

    Added `tox` to test environment

--- src/sensebook/__init__.py
+++ src/sensebook/__init__.py
@@ -1,10 +1,7 @@
 __title__ = "SenseBook"
 __version__ = "0.0.1"
 __description__ = "Making sense of Facebooks undocumented API"
-
-__copyright__ = "Copyright 2018 by Mads Marquart"
 __license__ = "BSD 3-Clause"
-
 __author__ = "Mads Marquart"
 __email__ = "madsmtm@gmail.com"
 
