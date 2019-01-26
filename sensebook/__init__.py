8b94d5058a68357b4919c9afaaca38f16bd2d871
Mads Marquart <madsmtm@gmail.com>
2019-01-26 13:49:43 +0100

    Move sansio implementation into primary module

--- sensebook/__init__.py
+++ sensebook/__init__.py
@@ -1,9 +1,19 @@
 """Making sense of Facebooks undocumented API."""
 
-__title__ = "SenseBook"
 __version__ = "0.1.2"
 
+from ._utils import (
+    default_user_agent,
+    parse_form,
+    build_url,
+    strip_json_cruft,
+    load_json,
+    time_from_millis,
+    random_hex,
+)
+from ._abc import State, ABCRequest
+
+from ._login import LoginError
+from ._listen import ProtocolError, PullRequest, Listener
+
 __all__ = ()
-
-
-from .sansio import State, LoginError, ProtocolError, Listener
